e8250ef39fd8a3be436160249cc94ab4349c5483
Andrelg1995 <andre.lg1995@aluno.ifsc.edu.br>
2020-12-02 20:20:55 -0300

    Versão atualizada 2.0

--- Python/Projeto_Imc.py
+++ Python/Projeto_Imc.py
@@ -1,11 +1,4 @@
 #projeto de programa IMC
-
-
-
-
-
-
-
 print("Vamos Medir seu IMC?")
 nome = str(input("Informe Seu Nome!: "))
 altura = float(input("Infome sua altura nesse formato 1.70!: "))
@@ -16,25 +9,27 @@ valaltura = altura*altura
 resultado = peso/valaltura
 
 print("Seu IMC é: {:.2f} ".format(resultado))
+if idade <= 60:
+    if resultado >= 40:
+        print("Obesidade grau 3 Procure um medico Seu Imc:{:.2f} ".format(resultado))
+    elif resultado >= 35:
+        print("Obesidade grau 2 Procure um medico Seu Imc:{:.2f} ".format(resultado))
+    elif resultado >= 30:
+        print("Obesidade grau 1 Procure um medico Seu Imc:{:.2f} ".format(resultado))
+    elif resultado >= 25:
+        print("Sobrepeso Procure um medico Seu Imc:{:.2f} ".format(resultado))
+    elif resultado >= 18.5:
+        print("Peso normal Seu Imc:{:.2f} ".format(resultado))
+    elif resultado < 18.5:
+        print("Abaixo do peso Seu Imc:{:.2f} ".format(resultado))
+else:
+    if resultado >= 27:
+        print("Idoso Obesidade Procure um medico Seu Imc:{:.2f} ".format(resultado))
+    elif resultado >= 22:
+        print("Idoso peso normal Seu Imc:{:.2f} ".format(resultado))
+    elif resultado < 22:
+        print("Idoso abaixo peso Seu Imc:{:.2f} ".format(resultado))
 
-if idade <= 60 and resultado >= 40:
-    print("Obesidade grau 3 Procure um medico Seu Imc:{:.2f} ".format(resultado))
-elif idade <= 60 and resultado >= 35:
-    print("Obesidade grau 2 Procure um medico Seu Imc:{:.2f} ".format(resultado))
-elif idade <= 60 and resultado >= 30:
-    print("Obesidade grau 1 Procure um medico Seu Imc:{:.2f} ".format(resultado))
-elif idade <= 60 and resultado >= 25:
-    print("Sobrepeso Procure um medico Seu Imc:{:.2f} ".format(resultado))
-elif idade <= 60 and resultado >= 18.5:
-    print("Peso normal Seu Imc:{:.2f} ".format(resultado))
-elif idade <= 60 and resultado < 18.5:
-    print("Abaixo do peso Seu Imc:{:.2f} ".format(resultado))
-elif idade > 60 and resultado >= 27:
-    print("Idoso Obesidade Procure um medico Seu Imc:{:.2f} ".format(resultado))
-elif idade > 60 and resultado >= 22:
-    print("Idoso peso normal Seu Imc:{:.2f} ".format(resultado))
-elif idade > 60 and resultado < 22:
-    print("Idoso abaixo peso Seu Imc:{:.2f} ".format(resultado))
 
 
 
